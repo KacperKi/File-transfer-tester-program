8be0c608d7bfd30d982983ef2f4ab64b2bb6bdfa
KacperKi <kacperxawer.123@gmail.com>
2023-06-17 17:14:36 +0200

    Added jfield to insert n- value to use in loop. Its important to improve accurancy of results

--- src/MyPanel.java
+++ src/MyPanel.java
@@ -1,26 +1,31 @@
 import java.awt.*;
 import java.awt.event.*;
 import java.io.File;
+import java.io.RandomAccessFile;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import javax.swing.*;
+import javax.swing.border.Border;
 import javax.swing.event.*;
 
 import static java.lang.Thread.sleep;
 
 public class MyPanel extends JPanel {
     private static JFrame frame;
+    Thread threadsObserver;
+    String pathToFile; boolean fileExistStop = false;
     private JLabel jcomp1,jcomp4,jcomp10,pathToTest, jcomp15;
-    private JCheckBox multipleThreadsEnable, fileSizeEnable,accept;
-    private JTextField numberOfThreads;
+    private JCheckBox multipleThreadsEnable, fileSizeEnable,accept, repeatsEnable;
+    private JTextField numberOfThreads, numberOfRepeat;
     private JSlider fileSizeSelect;
     private JButton startTestButton, selectTestingButton, selectFolderForResult;
     private JTextField numberOfThreadInformation, lopgInfoArea, selectedPathToTestingFolder, selectedPathForResult;
     private int threadsNumber = 1;
     private int activeThreads = 0;
     private int closed = 1;
-
+    float resultPercent = (float) 0.0;
+    boolean testWasFinished = false;
 
     public MyPanel() {
         createPanel();
@@ -31,13 +36,15 @@ public class MyPanel extends JPanel {
 
 
     }
-
     void createPanel(){
         //construct components
         jcomp1 = new JLabel ("NAS/FTP/Disc Tester - inż. K. Kisielewski - 2023, Lublin");
         multipleThreadsEnable = new JCheckBox ("Multiple Threads");
+        repeatsEnable = new JCheckBox ("n - repeate");
+
         numberOfThreads = new JTextField (5);
-        jcomp4 = new JLabel ("Number of Threads");
+        numberOfRepeat = new JTextField(5);
+        jcomp4 = new JLabel ("Nr Threads");
         fileSizeEnable = new JCheckBox ("Specify size of file");
         fileSizeSelect = new JSlider (0, 50);
         startTestButton = new JButton ("Start Test");
@@ -54,6 +61,9 @@ public class MyPanel extends JPanel {
 
         //set components properties
         numberOfThreads.setEnabled (false);
+        numberOfThreads.setText("1");
+        numberOfRepeat.setEnabled(false);
+        numberOfRepeat.setText("10");
         fileSizeSelect.setOrientation (JSlider.HORIZONTAL);
         fileSizeSelect.setMinorTickSpacing (10);
         fileSizeSelect.setMajorTickSpacing (3);
@@ -69,6 +79,7 @@ public class MyPanel extends JPanel {
         accept.setToolTipText ("Accept the terms and risks");
         selectedPathToTestingFolder.setToolTipText ("Here is path to selected folder.");
         selectedPathForResult.setToolTipText ("Here is PATH to your result.");
+        numberOfRepeat.setToolTipText("Type here int - n. It's important to improve accurancy of results.");
 
 
         //adjust size and set layout
@@ -93,12 +104,16 @@ public class MyPanel extends JPanel {
         add (jcomp15);
         add (selectFolderForResult);
         add (selectedPathForResult);
+        add (repeatsEnable);
+        add (numberOfRepeat);
 
         //set component bounds (only needed by Absolute Positioning)
         jcomp1.setBounds (5, 5, 320, 20);
+        repeatsEnable.setBounds(185,35,135,20);
         multipleThreadsEnable.setBounds (0, 35, 135, 20);
-        numberOfThreads.setBounds (180, 60, 100, 25);
-        jcomp4.setBounds (60, 60, 120, 25);
+        numberOfThreads.setBounds (85, 60, 100, 25);
+        numberOfRepeat.setBounds(185,60,100,25);
+        jcomp4.setBounds (5, 60, 100, 25);
         fileSizeEnable.setBounds (0, 90, 150, 20);
         fileSizeSelect.setBounds (5, 110, 315, 60);
         startTestButton.setBounds (5, 300, 100, 25);
@@ -148,9 +163,23 @@ public class MyPanel extends JPanel {
                     numberOfThreads.setText("");
                     threadsNumber = 1;
                 }
+                numberOfThreadInformation.setText("Active " + activeThreads + "/" + threadsNumber);
+
+            }
+        });
+        numberOfThreads.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                if(numberOfThreads.getText().equals(""))numberOfThreads.setText("1");
             }
         });
         fileSizeSelect.addChangeListener(e -> {
+            if(fileSizeSelect.getValue() ==0 ) fileSizeSelect.setValue(1);
             lopgInfoArea.setText("New size of file: " + fileSizeSelect.getValue() + " MB");
             fileSizeSelect.setToolTipText (fileSizeSelect.getValue() + " MB");
         });
@@ -174,31 +203,83 @@ public class MyPanel extends JPanel {
                 selectedPathForResult.setText("C:\\Users");
             }
         });
+        repeatsEnable.addItemListener(e -> {
+            numberOfRepeat.setEnabled(e.getStateChange() == ItemEvent.SELECTED);
+            if(e.getStateChange() == ItemEvent.DESELECTED) numberOfRepeat.setText("10");
+        });
+        numberOfRepeat.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                if(Integer.parseInt(numberOfRepeat.getText() )> 0 && Integer.parseInt(numberOfRepeat.getText()) < 10001 )numberOfRepeat.setText("10");
+            }
+        });
 
         startTestButton.addActionListener(e -> {
             testAllElements();
+            resultPercent = 0;
+            activeThreads = 0;
+            testWasFinished = false;
+
             while(closed == 1){
                 //DO NTH
-                System.out.println(closed);
                 try{
                     sleep(500);
                 }catch(Exception we){
 
                 }
             }
-
             if(closed == 0){
-                runThreads();
+                resultPercent = (float) 0;
+                activeThreads = 0;
+
+                generateRandomFile();
+
+                if(!fileExistStop) {
+                    observeThreadsNumber();
+                    runInfoStatusMonitor();
+                    runThreads();
+                }
             }
-
-
         });
 
     }
 
+    void runInfoStatusMonitor(){
+        JFrame f = new JFrame("Status Test Monitor");
+        f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
+        Container content = f.getContentPane();
+        JProgressBar progressBar = new JProgressBar();
 
+        progressBar.setValue(0);
+        progressBar.setStringPainted(true);
+        Border border = BorderFactory.createTitledBorder("Testing Server...");
 
+        progressBar.setBorder(border);
+
+        content.add(progressBar, BorderLayout.NORTH);
+        f.setSize(300, 100);
+        f.setVisible(true);
+
+        Thread refreshingStat = new Thread("Watcher") {
+            public void run(){
+                while(!testWasFinished){
+                    progressBar.setValue((int) getResultPercent());
+                }
+                progressBar.setValue(100);
+                Border border = BorderFactory.createTitledBorder("Testing finished!");
+                progressBar.setBorder(border);
+
+            }
+        };
+        refreshingStat.start();
+
+    }
     boolean testAllElements(){
         Path pathTesting = Path.of(selectedPathToTestingFolder.getText());
         Path pathResult = Path.of(selectedPathForResult.getText());
@@ -214,7 +295,6 @@ public class MyPanel extends JPanel {
                     null,
                     null
             );
-            System.out.println("W" + closed);
             return true;
         }
         else {
@@ -231,6 +311,73 @@ public class MyPanel extends JPanel {
             return false;
         }
     }
+    void observeThreadsNumber(){
+        threadsObserver = new Thread("Thread Number Watcher") {
+            public void run(){
+                while(!testWasFinished){
+                    int threadDone = threadsNumber - activeThreads;
+                    setResultPercent((float) (( (float) threadDone / (float) threadsNumber ) * 100.0));
+                }
+            }
+        };
+        try{
+            sleep(1000);
+        } catch(Exception e){}
+        threadsObserver.start();
+    }
+
+    public float getResultPercent() {
+        return resultPercent;
+    }
+    public void setResultPercent(float resultPercent) {
+        this.resultPercent = resultPercent;
+    }
+
+
+    void generateRandomFile(){
+        String fileName = "\\test.txt";
+        pathToFile = selectedPathToTestingFolder.getText() + fileName;
+        boolean dialogShow = false;
+
+        if(Files.exists(Path.of(pathToFile))) {
+            dialogShow = true;
+            int result = JOptionPane.showConfirmDialog(frame,"Sure? You want to overide file?", "Warning! File exists!",
+                    JOptionPane.YES_NO_OPTION,
+                    JOptionPane.QUESTION_MESSAGE);
+
+            if(result == JOptionPane.YES_OPTION){
+                File file = new File(pathToFile);
+                file.delete();
+
+                while(Files.exists(Path.of(pathToFile))){} //wait for remove file
+                fileExistStop = false;
+
+            }else if (result == JOptionPane.NO_OPTION){
+                fileExistStop = true;
+            }else {
+                fileExistStop = true;
+            }
+            dialogShow = false;
+        }
+
+        while(dialogShow){}
+
+        if(!fileExistStop) {
+            File file = new File(pathToFile);
+            RandomAccessFile randomAccessFile;
+            try {
+                randomAccessFile = new RandomAccessFile(file, "rw");
+                randomAccessFile.setLength(ConertMbToBytes(fileSizeSelect.getValue()));
+                randomAccessFile.close();
+            } catch (Exception e) {}
+        }
+        while(!Files.exists(Path.of(pathToFile)) && !fileExistStop){}
+
+    }
+    int ConertMbToBytes(int numberMb){
+        return numberMb * 1048576;
+
+    }
 
     void runThreads(){
         ArrayList<Runnable> listOfTh = new ArrayList<>();
@@ -239,23 +386,25 @@ public class MyPanel extends JPanel {
             int finalI = i;
             Thread thread = new Thread("New Thread no. " + finalI) {
                 public void run(){
-                    System.out.println("Runnable running");
                     activeThreads++;
                     numberOfThreadInformation.setText("Active " + activeThreads + "/" + threadsNumber);
 
                     try{sleep(1000 + 100 * finalI);}catch (Exception e){}
-                    System.out.println("Runnable stoped");
 
                     activeThreads--;
                     numberOfThreadInformation.setText("Active " + activeThreads + "/" + threadsNumber);
-                    System.out.println("Size of table: " + listOfTh.size());
+
+
+
+                    if(activeThreads == 1) {
+                        testWasFinished = true;
+                    }
                 }
             };
             listOfTh.add(thread);
             thread.start();
         }
     }
-
 
     public static void main (String[] args) {
         frame = new JFrame ("Fail Transfer Tester v1.0");
